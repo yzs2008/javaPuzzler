7fc90984e37df4e78ed436c11d9537a15c100f83
yzs2008 <yzszzu@gmail.com>
2015-05-05 16:53:17 +0800

    java puzzler project initialized.

---
+++ JavaPuzzlers/src/com/java/puzzlers/puzzle8/BaseClass.java
@@ -0,0 +1,12 @@
+package com.java.puzzlers.puzzle8;
+
+public class BaseClass {
+	public int count;
+	public int x;
+	public void method(){
+		System.out.println("base method");
+	}
+	public void baseMethod(){
+		System.out.println("this is base method");
+	}
+}
---
+++ JavaPuzzlers/src/com/java/puzzlers/puzzle8/NotInitializeClass.java
@@ -0,0 +1,12 @@
+package com.java.puzzlers.puzzle8;
+
+public class NotInitializeClass {
+
+	public static void main(String[] args){
+//		System.out.println(SubClass.value);
+		SubClass sc=new SubClass();
+		System.out.println(sc.value);
+		SuperClass[] sca=new SubClass[10];
+//		System.out.println(SubClass.value);
+	}
+}
