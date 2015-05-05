355a6fa4658ad25a4cbf54c3ea874e43b8a0390d
yzs2008 <yzszzu@gmail.com>
2015-05-05 20:00:11 +0800

    Long division suffix L to integer convert to long

---
+++ JavaPuzzlers/src/com/java/puzzlers/expressive/LongDivision.java
@@ -0,0 +1,23 @@
+package com.java.puzzlers.expressive;
+/**
+ * 
+ * @author kaidi
+ *
+ */
+public class LongDivision {
+
+	public static void main(String[] args) {
+		final long longMax=Long.MAX_VALUE;
+		System.out.println(longMax);
+		System.out.println(Integer.MAX_VALUE);
+		//the computation is performed entirely in integer arithmetic
+		//and actually overflow.
+		final long micro_per_day=24*60*60*1000*1000;
+		//to fix the problem, use L to suffix the integer, just one is enough
+		final long micro_per_day_fixed=24*60*60*1000*1000L;
+		final long millis_per_day=24*60*60*1000;
+		System.out.println(micro_per_day/millis_per_day);
+		System.out.println(micro_per_day_fixed/millis_per_day);
+	}
+
+}
---
+++ JavaPuzzlers/src/com/java/puzzlers/expressive/TimeForChange.java
@@ -0,0 +1,15 @@
+package com.java.puzzlers.expressive;
+/**
+ * 
+ * @author kaidi
+ *
+ */
+public class TimeForChange {
+
+	public static void main(String[] args){
+		System.out.println(2.00-1.10);
+		System.out.println(2.00-1.20);
+		System.out.println(2.00-1.30);
+		System.out.println(2.00-1.40);
+	}
+}
