b7fad5830008eeacf7a7d77d6da163a39e53922a
yzs2008 <yzs2008@yeah.net>
2015-11-17 16:25:23 +0800

    git 分支创建联系

--- JavaPuzzlers/src/com/java/puzzlers/puzzle8/CreateNewBranchTest.java
+++ JavaPuzzlers/src/com/java/puzzlers/puzzle8/CreateNewBranchTest.java
@@ -1,10 +1,14 @@
 package com.java.puzzlers.puzzle8;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CreateNewBranchTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Map<String,String> theOne=new HashMap<String, String>();
+		theOne.put("key", "value");
 	}
 
 }
