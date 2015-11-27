7b8b73808209f34d828e3fc927aedf66d24a940d
yzs2008 <yzs2008@yeah.net>
2015-11-27 12:21:22 +0800

    test2 for git

--- JavaPuzzlers/src/com/java/puzzlers/expressive/DateTimeCompare.java
+++ JavaPuzzlers/src/com/java/puzzlers/expressive/DateTimeCompare.java
@@ -5,9 +5,9 @@ import java.util.Date;
 public class DateTimeCompare {
 
   public static void main(String[] args) {
-  Date dtA=new Date(2015,10,12);
-  Date dtB=new Date(2015,11,12);
-    
+    Date dtA = new Date(2015, 10, 12);
+    Date dtB = new Date(2015, 11, 12);
+
     System.out.println(dtA.before(dtB));
   }
 }
