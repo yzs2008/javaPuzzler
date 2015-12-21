303abcd080437dd021fb7a9c7cc11b69b5a60377
yzs2008 <yzs2008@yeah.net>
2015-12-21 19:37:01 +0800

    上班的积累

---
+++ JavaPuzzlers/src/com/java/puzzlers/expressive/DoubleEqualZero.java
@@ -0,0 +1,12 @@
+package com.java.puzzlers.expressive;
+
+public class DoubleEqualZero {
+  
+  public  static void main(String[] args){
+    Double d=0.11D;
+    System.out.println(d==0);
+    Double dzero= 0.000D;
+    System.out.println(dzero==0);
+  }
+
+}
