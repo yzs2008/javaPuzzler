668c856e84f96616f021604250c3b4fc162042b7
yzs2008 <yzs2008@yeah.net>
2016-02-26 20:50:44 +0800

    format my code style with my favor

--- JavaPuzzlers/test/kaidi/ominprime/unit/Lambda.java
+++ JavaPuzzlers/test/kaidi/ominprime/unit/Lambda.java
@@ -1,6 +1,7 @@
 package kaidi.ominprime.unit;
 
 import kaidi.base.MockTest;
+import main.ominprime.unit.RepaymentStatus;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -20,37 +21,53 @@ import java.util.stream.IntStream;
  */
 public class Lambda extends MockTest {
 
-  @Test
-  public void testIntStream() {
-    List<Integer> integerList = IntStream.of(10, 30, 40, 100, 200, 500, 600)
-                                        .boxed()
-                                        .collect(Collectors.toList());
-    List<Integer> originList = new ArrayList<>();
-    originList.add(10);
-    originList.add(30);
-    originList.add(40);
-    originList.add(100);
-    originList.add(200);
-    originList.add(500);
-    originList.add(600);
+    @Test
+    public void testIntStream() {
+        List<Integer> integerList = IntStream.of(10, 30, 40, 100, 200, 500, 600)
+                                             .boxed()
+                                             .collect(Collectors.toList());
+        List<Integer> originList = new ArrayList<>();
+        originList.add(10);
+        originList.add(30);
+        originList.add(40);
+        originList.add(100);
+        originList.add(200);
+        originList.add(500);
+        originList.add(600);
 
-    Assert.assertEquals(originList, integerList);
-  }
+        Assert.assertEquals(originList, integerList);
+    }
 
-  @Test
-  public void testStreamPartitionBy() {
-    List<Integer> integerList = IntStream.of(10, 30, 40, 100, 200, 500, 600)
-                                        .boxed()
-                                        .collect(Collectors.toList());
-    Map<Boolean, List<Integer>> result = integerList.stream().collect(Collectors.partitioningBy(x -> x >= 100));
-    List<Integer> lt100List = IntStream.of(10, 30, 40)
-                                      .boxed()
-                                      .collect(Collectors.toList());
-    List<Integer> gt100List = IntStream.of(100, 200, 500, 600)
-                                      .boxed()
-                                      .collect(Collectors.toList());
-    Assert.assertEquals(gt100List,result.get(true));
-    Assert.assertEquals(lt100List,result.get(false));
-  }
+    @Test
+    public void testStreamPartitionBy() {
+        List<Integer> integerList = IntStream.of(10, 30, 40, 100, 200, 500, 600)
+                                             .boxed()
+                                             .collect(Collectors.toList());
+        Map<Boolean, List<Integer>> result = integerList.stream().collect(Collectors.partitioningBy(x -> x >= 100));
+        List<Integer> lt100List = IntStream.of(10, 30, 40)
+                                           .boxed()
+                                           .collect(Collectors.toList());
+        List<Integer> gt100List = IntStream.of(100, 200, 500, 600)
+                                           .boxed()
+                                           .collect(Collectors.toList());
+        Assert.assertEquals(gt100List, result.get(true));
+        Assert.assertEquals(lt100List, result.get(false));
+    }
+
+    public void testFunctionReference() {
+        List<Integer> integerList = IntStream.of(10, 30, 40, 100, 200, 500, 600)
+                                             .boxed()
+                                             .collect(Collectors.toList());
+        Map<Boolean, List<Integer>> result = integerList.stream()
+                                                        .collect(Collectors.partitioningBy(RepaymentStatus::isRepaymentStatus));
+        List<Integer> lt100List = IntStream.of(10, 30, 40)
+                                           .boxed()
+                                           .collect(Collectors.toList());
+        List<Integer> gt100List = IntStream.of(100, 200, 500, 600)
+                                           .boxed()
+                                           .collect(Collectors.toList());
+        Assert.assertEquals(gt100List, result.get(true));
+        Assert.assertEquals(lt100List, result.get(false));
+    }
 }
 
