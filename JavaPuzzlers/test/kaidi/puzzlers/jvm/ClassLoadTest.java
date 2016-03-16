fafeb9692e263ca0ae174a1262d361440b07709f
yzs <yzs2008@yeah.net>
2016-03-16 20:56:35 +0800

    start analysis class loader

---
+++ JavaPuzzlers/test/kaidi/puzzlers/jvm/ClassLoadTest.java
@@ -0,0 +1,17 @@
+package kaidi.puzzlers.jvm;
+
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kaidi
+ * Date: 2016/3/11
+ * Time: 16:27
+ * Write the code, Change the world.
+ */
+public class ClassLoadTest {
+  @Test
+  public void testClassLoad() {
+
+  }
+}
