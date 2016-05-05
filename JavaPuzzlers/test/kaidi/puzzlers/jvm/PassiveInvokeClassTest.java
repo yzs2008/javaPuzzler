e7788412820f39057be4340f60ad305f0432da68
yzs2008 <yzs2008@yeah.net>
2016-05-05 11:12:23 +0800

    reform builder pattern

--- JavaPuzzlers/test/kaidi/puzzlers/jvm/PassiveInvokeClassTest.java
+++ JavaPuzzlers/test/kaidi/puzzlers/jvm/PassiveInvokeClassTest.java
@@ -17,14 +17,14 @@ import java.util.Map;
 public class PassiveInvokeClassTest {
   /**
    * A use of a non-constant field is an active use of only the class or
-   interface that actually declares the field. For example, a field declared
-   in a class may be referred to via a subclass. A field declared in an
-   interface may be referred to via a subinterface or class that implements
-   the interface. These are passive uses of the subclass, subinterface, or
-   class that implements the interface--uses that wonít trigger their
-   initialization. They are an active use only of the class or interface in
-   which the field is actually declared. Here is an example that illustrates
-   this principle:
+   * interface that actually declares the field. For example, a field declared
+   * in a class may be referred to via a subclass. A field declared in an
+   * interface may be referred to via a subinterface or class that implements
+   * the interface. These are passive uses of the subclass, subinterface, or
+   * class that implements the interface--uses that wonít trigger their
+   * initialization. They are an active use only of the class or interface in
+   * which the field is actually declared. Here is an example that illustrates
+   * this principle:
    */
   @Test
   public void testPassiveInvoke() {
